17d155e246d233b149b324f14598ead2e2df36c4
VICTOR PONTUAL GUEDES ARRUDA NOBREGA <victorpontualgan@gmail.com>
2025-10-22 12:29:39 -0300

    refactor: atualizando BaseScreen para estar condizendo com o estado atual

--- src/Template/BaseScreen.py
+++ src/Template/BaseScreen.py
@@ -8,20 +8,15 @@ class BaseScreen:
     """
 
     def __init__(self):
-        self.surface = ScreenManager.get_tela()
-        self.relogio = ScreenManager.get_relogio()
-        self.assets = None
         self._is_open = False
 
     def open_screen(self):
         """Chamado quando a screen é aberta (Facade Pattern)."""
         self._is_open = True
-        self.on_enter()
     
     def close_screen(self):
         """Chamado quando a screen é fechada (Facade Pattern)."""
         self._is_open = False
-        self.on_exit()
     
     def is_open(self) -> bool:
         """Retorna se a screen está aberta."""
@@ -37,12 +32,4 @@ class BaseScreen:
 
     def draw(self, surface: pygame.Surface):
         """Desenha conteúdo da tela."""
-        pass
-
-    def on_enter(self, **kwargs):
-        """Chamado quando a tela se torna ativa."""
-        pass
-
-    def on_exit(self):
-        """Chamado quando a tela deixa de estar ativa."""
         pass
